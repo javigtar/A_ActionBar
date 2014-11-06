83208dd14ed92ee96b49f988d50caa66cdd5f9ae
Javi <javi>
2014-11-06 19:49:58 +0100

    acabado

--- app/src/main/java/dam/org/actionbar/CirculoFragment.java
+++ app/src/main/java/dam/org/actionbar/CirculoFragment.java
@@ -1,10 +1,15 @@
 package dam.org.actionbar;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * Created by alumno on 06/11/2014.
@@ -18,4 +23,25 @@ public class CirculoFragment extends Fragment {
         return inflater.inflate(R.layout.circulo_fragment, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayout layout = (LinearLayout) view.findViewById(R.id.circuloLayout);
+        layout.addView(new Circulo(view.getContext()));
+    }
+
+    public class Circulo extends View{
+
+        public Circulo(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            Paint pincel = new Paint();
+            pincel.setColor(Color.BLUE);
+            canvas.drawCircle(150,200,100,pincel);
+        }
+    }
 }
--- app/src/main/java/dam/org/actionbar/CuadradoFragment.java
+++ app/src/main/java/dam/org/actionbar/CuadradoFragment.java
@@ -5,19 +5,55 @@ package dam.org.actionbar;
  */
 
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class CuadradoFragment extends Fragment {
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.cuadrado_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayout relativeLayout = (LinearLayout) view.findViewById(R.id.cuadradoLayout);
+        relativeLayout.addView(new Cuadrado(getActivity()));
+
+    }
+
+    private class Cuadrado extends View{
+
+        public Cuadrado(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            Paint paint = new Paint();
+            paint.setColor(Color.BLUE);
+            Rect rect = new Rect(100, 100, 200, 200);
+            canvas.drawRect(rect, paint);
+        }
     }
 
 }
